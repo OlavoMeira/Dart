93225e77589d3deda89ba632b87a488ed53f9de6
Olavo Barros Meira Filho <olavomeiradeveloper@gmail.com>
2025-03-24 17:28:29 -0300

    tipagem
    tipos e variaveis

---
+++ dart_application_3/bin/server.dart
@@ -0,0 +1,13 @@
+void main( List<String> arguments) {
+  String texto = "DIO";
+  var texto2 = "DIO";
+
+  int numero1 = 1;
+  var numero2 = 2;
+  int numero3;
+
+  List<String> lista = [];
+  var lista1 = [];
+  int lista2;
+
+}
