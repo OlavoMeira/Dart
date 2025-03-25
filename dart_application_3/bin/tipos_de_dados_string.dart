5a4f97d7f804f52dc22862a2d5b50c43eb47b05b
Olavo Barros Meira Filho <olavomeiradeveloper@gmail.com>
2025-03-25 08:46:05 -0300

    tipo string

--- dart_application_3/bin/tipos_de_dados_string.dart
+++ dart_application_3/bin/tipos_de_dados_string.dart
@@ -16,5 +16,30 @@ void main(List<String> arguments) {
   print(texto2.length);
 
   // Maiuscula / Minuscula.
-  print()
-}
+  print(texto1.toUpperCase());
+  print(texto2.toLowerCase());
+
+  //se uma string contem na outra ou seja se uma parte da string esta dentro da outra.
+  print(texto2.contains(texto1));
+
+  // obtem parte da String ou seja posso especificar o que saira no print.
+  print(texto2.substring(5));
+  print(texto2.substring(1, 5));
+
+  // Obtem a posição de um texto em uma String.
+  print(texto2.indexOf("Fluter"));
+  print(texto2.indexOf("T"));
+  print(texto2.indexOf("@"));
+
+  // substitui uma String em outra.
+  print(texto2.replaceAll("a", "@"));
+
+  // Quebra uma String por um caracter especifico.
+  print(texto2.split("a"));
+  print("NOME;ENDEREÇO;CEP".split(";"));
+
+  // Remove espaços.
+  print(" dio ".trim());
+  print(" dio ".trimLeft());
+  print(" dio ".trimRight());
+} 
