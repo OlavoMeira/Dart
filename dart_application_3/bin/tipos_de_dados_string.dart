19bd103f6f399b9a1031be3a272210397e291c22
Olavo Barros Meira Filho <olavomeiradeveloper@gmail.com>
2025-03-24 22:51:08 -0300

    tipos
    tipos

--- dart_application_3/bin/tipos_de_dados_string.dart
+++ dart_application_3/bin/tipos_de_dados_string.dart
@@ -1,4 +1,20 @@
 void main(List<String> arguments) {
-  String texto1 = "Dio.me"
+  String texto1 = "Dio.me";
+  var texto2 = "Trilha de Flutter e Dart na dio.me";
 
+
+  // imprime no console o texto.
+  print(texto1);
+  print(texto2);
+
+  // Comparação de vazio.
+  print(texto2.isEmpty);
+  print("".isEmpty);
+  print(" ".isEmpty);
+
+  // Obtem o tamanho da String.
+  print(texto2.length);
+
+  // Maiuscula / Minuscula.
+  print()
 }
