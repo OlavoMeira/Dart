573d66c3e329f5339c5dbb72c67a958d248a10ef
Olavo Barros Meira Filho <olavomeiradeveloper@gmail.com>
2025-03-27 15:11:28 -0300

    Update lend0_dados_do_console.dart

--- dart_application_3/bin/ Estruturas Condicionais e de Repetição/lend0_dados_do_console.dart
+++ dart_application_3/bin/ Estruturas Condicionais e de Repetição/lend0_dados_do_console.dart
@@ -4,17 +4,17 @@ import 'dart:io';
 void main(List<String> arguments) {
   print("Bem vindos a nossa calculadora!");
   print("Informe o primeiro número: ");
-  var line = stdin.readLineSync((encoding: utf8));
-  var numero1 = double.parse(line ?? "0");
+  var line = stdin.readLineSync(encoding: utf8);
+  double numero1 = double.parse(line ?? "0"); // Correção: Tipo definido para numero1
 
-  print("imforme o segundo numero: ");
+  print("Informe o segundo número: "); // Correção: "imforme" para "Informe"
   line = stdin.readLineSync(encoding: utf8);
-  var numero2 = int.parse(line ?? "0");
-  
+  double numero2 = double.parse(line ?? "0"); // Correção: Tipo definido para numero2
 
-  print("Informe a operação matematica (+, -, *, /):");
+
+  print("Informe a operação matemática (+, -, *, /):");
   line = stdin.readLineSync(encoding: utf8);
-  var operacao = line ?? "";
+  String operacao = line ?? ""; // Correção: Tipo definido para operacao
 
   print(numero1);
   print(numero2);
