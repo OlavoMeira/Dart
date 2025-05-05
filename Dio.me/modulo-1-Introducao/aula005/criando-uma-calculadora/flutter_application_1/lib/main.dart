80eb5d8dc7632b22ca7ebcddfc4dd815c96e12b3
Olavo Barros Meira Filho <olavomeiradeveloper@gmail.com>
2025-05-05 16:07:16 -0300

    calc2
    calc2

--- Dio.me/modulo-1-Introducao/aula005/criando-uma-calculadora/flutter_application_1/lib/main.dart
+++ Dio.me/modulo-1-Introducao/aula005/criando-uma-calculadora/flutter_application_1/lib/main.dart
@@ -7,26 +7,37 @@ import 'dart:convert';
 // com o void main estou iniciando a função OBRIGATORIA de abertura do codigo dart.
 
 void main (List<String> arguments){
-  print ("imforme a primeira nota: ");
+  print ("Bem vindos a nossa calculadora!");
+  print ("imforme o primeiro número: ");
 
 // declarando a variavel line e usando stdin.readLineSync estou pedindo para o dart para fazer input de dados via cconsole.
   
   var line = stdin.readLineSync(encoding: utf8);
-  var prova1 = int.parse(line ?? "0");
+  var numero1= double.parse(line ?? "0");
 
-print("Imforme a segunda nota: ");
+print("Imforme o segundo número: ");
 line = stdin.readLineSync(encoding: utf8);
-var prova2 = int.parse(line ?? "0");
+var numero2 = double.parse(line ?? "0");
+
+print("Imforme a operação matematica (+,-,*,/): ");
+line = stdin.readLineSync(encoding: utf8);
+var operacao = line ?? "";
 
 // termos e condicionais logo abaixo.
 
-var media =  (prova1 + prova2) / 2;
-if (media > 7) {
-  print("O aluno passou com a nota: $media");
-} else if (media >= 5) {
-  print("O aluno esta de recuperação com a nota: $media");
-} else {
-  print("O aluno reprovou com a nota: $media");
-}
-}
+double resultado = 0;
 
+if (operacao == "+") {
+  resultado = numero1 + numero2;
+} else if ( operacao == "-"){
+  resultado = numero1 - numero2;
+}else if (operacao == "*"){
+  resultado = numero1 * numero2;
+}else if (operacao == "/"){
+  resultado = numero1 / numero2;
+} else {
+  print("Operação invalida");
+  exit(0);
+}
+print("O resultado da operação é: $resultado");
+}
