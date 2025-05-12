3a843371e856241a97f30a05358ac047edd74a1c
Olavo Barros Meira Filho <olavomeiradeveloper@gmail.com>
2025-05-12 08:08:56 -0300

    ada
    dada

---
+++ dart_mini_projeto_UNEX/flutter_application_meuapp/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\CTI\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\CTI\Documents\GitHub\Dart\dart_mini_projeto_UNEX\flutter_application_meuapp"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
