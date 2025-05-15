ffba7d67531f512bb74a3f7401f1585bac47b566
Olavo Barros Meira Filho <olavomeiradeveloper@gmail.com>
2025-05-15 11:13:23 -0300

    primeira aula
    primeira aula

--- Dio.me/modulo-1-Introducao/Topico-02/aula001/Introdução/flutter_application_1/lib/main.dart
+++ Dio.me/modulo-1-Introducao/Topico-02/aula001/Introdução/flutter_application_1/lib/main.dart
@@ -1,3 +1,28 @@
 void main(List<String> arguments){
-  
+  printhelloworld();
+
+  printname("Danilo");
+
+  var number =  PrintNumber();
+
+  print(number);
+
+  var resultado = sum(10,40);
+  print(resultado);
+}
+
+void printhelloworld(){
+  print("Hellow World");
+}
+
+void printname(String name){
+  print("My name is: $name");
+}
+
+int PrintNumber(){
+  return 30;
+}
+
+int sum(int number1, int numeber2){
+  return number1 + numeber2;
 }
